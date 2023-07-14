3e5431f1c96caf50a10bdf01e5e55e389d451e20
Cristian morales <ccmj202@gmail.com.com>
2023-07-13 19:18:42 -0500

    🚀 Logger: Winston and middleware logger setup and ready

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,19 @@ import Koa from 'koa';
 import parser from 'koa-parser'
 
 import mainRouter from './routes/main/index.js'
+import { middlewareLogger } from './logger/middleware.js';
+import { logger } from './logger/logger.js';
 
 const API_PORT = 3000
 const app = new Koa();
 
 app.use(parser())
 
+app.use(middlewareLogger());
+
 // ROUTERS
 app.use(mainRouter.routes()).use(mainRouter.allowedMethods())
 
 app.listen(API_PORT, () => {
-  console.log(`API ready, up and running on port: ${API_PORT}`)
+  logger.info(`🚀 Server ready at: http://localhost: ${API_PORT} 🚀\n\t⭐️⭐️⭐️⭐️⭐️`)
 });
