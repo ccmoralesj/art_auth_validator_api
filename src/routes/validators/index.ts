eee33fc1a9eab08e8949db072a82041f73a8cb54
Cristian morales <ccmj202@gmail.com.com>
2023-11-28 23:40:43 -0500

    ⚙️  Core: Eslit corrections

--- src/routes/validators/index.ts
+++ src/routes/validators/index.ts
@@ -1,97 +1,107 @@
-import { Context, Next } from 'koa';
-import Router from 'koa-router';
-import { SECRET_TAG_BASE } from '../../helpers/consts/index.js';
-import { stringToHexString } from '../../helpers/stringToHex.js';
-import { generateQR, generateQRFileName } from '../../services/QRgenerator/index.js';
-import { checkValidationPrhase, createUUID, createValidationPrhase } from '../../helpers/crypto.js';
-import { AuthenticityCertificate, RouteStructure, checkValidationPhraseParams } from '../../types/index.js';
-import { logger } from '../../logger/logger.js';
-import { createCertificate, testingDB } from '../../schemas/Certificates.js';
+import { Context, Next } from "koa";
+import Router from "koa-router";
+import { SECRET_TAG_BASE } from "../../helpers/consts/index.js";
+import { stringToHexString } from "../../helpers/stringToHex.js";
+import {
+  generateQR,
+  generateQRFileName,
+} from "../../services/QRgenerator/index.js";
+import {
+  checkValidationPrhase,
+  createUUID,
+  createValidationPrhase,
+} from "../../helpers/crypto.js";
+import {
+  AuthenticityCertificate,
+  RouteStructure,
+  checkValidationPhraseParams,
+} from "../../types/index.js";
+import { logger } from "../../logger/logger.js";
+import { createCertificate, testingDB } from "../../schemas/Certificates.js";
 
 const router = new Router();
-const ROUTE_BASE = '/validators'
+const ROUTE_BASE = "/validators";
 
-export const allRoutes: RouteStructure  = {
+export const allRoutes: RouteStructure = {
   [ROUTE_BASE]: {
     path: ROUTE_BASE,
-    method: 'GET',
-    auth: false
+    method: "GET",
+    auth: false,
   },
   [`${ROUTE_BASE}/generate-qr`]: {
     path: `${ROUTE_BASE}/generate-qr`,
-    method: 'GET',
-    auth: true
+    method: "GET",
+    auth: true,
   },
   [`${ROUTE_BASE}/generate-validator`]: {
     path: `${ROUTE_BASE}/generate-validator`,
-    method: 'POST',
-    auth: true
+    method: "POST",
+    auth: true,
   },
   [`${ROUTE_BASE}/check-authenticity`]: {
+    // TODO generate key only for FE to call this endpoint
     path: `${ROUTE_BASE}/check-authenticity`,
-    method: 'POST',
-    auth: false
+    method: "POST",
+    auth: false, // TODO change true for production
   },
-  [`${ROUTE_BASE}/validate-form`]: {
-    path: `${ROUTE_BASE}/validate-form`,
-    method: 'GET',
-    auth: false
-  }
-}
+};
 
 router.get(`${ROUTE_BASE}`, async (ctx: Context, _next: Next) => {
-  await testingDB()
+  await testingDB();
   ctx.body = {
-    msg: 'Hello Koa from validators'
+    msg: "Hello Koa from validators",
   };
 });
 
 router.get(`${ROUTE_BASE}/generate-qr`, async (ctx: Context, _next: Next) => {
-  const secret = `${stringToHexString(SECRET_TAG_BASE)}-${createUUID()}`
-  logger.info({ secret })
-  const QRGenerated = await generateQR(secret)
-  const QRCodeName = generateQRFileName()
-  ctx.response.set("content-type", 'image/svg+xml');
-  ctx.response.set('Content-disposition', `filename=${QRCodeName}`)
-  ctx.body = QRGenerated
+  const secret = `${stringToHexString(SECRET_TAG_BASE)}-${createUUID()}`;
+  logger.info({ secret });
+  const QRGenerated = await generateQR(secret);
+  const QRCodeName = generateQRFileName();
+  ctx.response.set("content-type", "image/svg+xml");
+  ctx.response.set("Content-disposition", `filename=${QRCodeName}`);
+  ctx.body = QRGenerated;
 });
 
-router.post(`${ROUTE_BASE}/generate-validator`, async (ctx: Context, _next: Next) => {
-  const { secret } = ctx.query
-  if (typeof secret !== 'string') {
-    ctx.response.status = 400
-    ctx.response.message = 'secret must be just one string'
-    return
+router.post(
+  `${ROUTE_BASE}/generate-validator`,
+  async (ctx: Context, _next: Next) => {
+    const { secret } = ctx.query;
+    if (typeof secret !== "string") {
+      ctx.response.status = 400;
+      ctx.response.message = "secret must be just one string";
+      return;
+    }
+    const certificateData = ctx.request.body as AuthenticityCertificate;
+    const { validationPhrase, pinToArt } = createValidationPrhase({
+      certificateData,
+      secret,
+    });
+    const newCertificate = await createCertificate({
+      artInfo: certificateData,
+      hash: validationPhrase,
+      secret,
+    });
+    logger.info({ newCertificate });
+
+    ctx.body = {
+      ...newCertificate,
+      pinToArt,
+    };
   }
-  const certificateData = ctx.request.body as AuthenticityCertificate
-  const {
-    validationPhrase,
-    pinToArt
-  } = createValidationPrhase({
-    certificateData,
-    secret
-  })
-  const newCertificate = await createCertificate({
-    artInfo: certificateData,
-    hash: validationPhrase,
-    secret,
-  })
-  logger.info({ newCertificate })
+);
 
-  ctx.body = {
-    ...newCertificate,
-    pinToArt
-  };
-});
+router.post(
+  `${ROUTE_BASE}/check-authenticity`,
+  async (ctx: Context, _next: Next) => {
+    const { secret, pin } = ctx.request.body as checkValidationPhraseParams;
 
-router.post(`${ROUTE_BASE}/check-authenticity`, async (ctx: Context, _next: Next) => {
-  const { secret, pin } = ctx.request.body as checkValidationPhraseParams
-
-  const isAuthentic = await checkValidationPrhase({ secret, pin })
-  const validationResponse = isAuthentic ? '✅ VALID ✅' : '❌ WRONG ❌'  
-  ctx.body = {
-    msg: `${validationResponse} Certificate of Authenticity`
-  };
-});
+    const isAuthentic = await checkValidationPrhase({ secret, pin });
+    const validationResponse = isAuthentic ? "✅ VALID ✅" : "❌ WRONG ❌";
+    ctx.body = {
+      msg: `${validationResponse} Certificate of Authenticity`,
+    };
+  }
+);
 
 export default router;
--- src/services/QRgenerator/index.ts
+++ src/services/QRgenerator/index.ts
@@ -1,31 +1,30 @@
-import { makeRequestRaw } from "../../helpers/fecthRequest.js"
-import { randomIntFromInterval } from "../../helpers/randomNumberBetween.js"
-import { logger } from "../../logger/logger.js"
-import { allRoutes as validatorRoutes } from "../../routes/validators/index.js"
-import { QR_GENERATOR_API_URL } from "./consts.js"
+import { makeRequestRaw } from "../../helpers/fecthRequest.js";
+import { randomIntFromInterval } from "../../helpers/randomNumberBetween.js";
+import { logger } from "../../logger/logger.js";
+import { QR_GENERATOR_API_URL } from "./consts.js";
 
 export function generateQRFileName() {
-  const now = new Date()
-  const stringDateFormat = `${now.getMonth() + 1}-${now.getDate()}-${now.getFullYear()}`
-  const stringDateTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
-  return `generated-qr-${randomIntFromInterval(100,999)}-${stringDateFormat}-${stringDateTime}`
+  const now = new Date();
+  const stringDateFormat = `${
+    now.getMonth() + 1
+  }-${now.getDate()}-${now.getFullYear()}`;
+  const stringDateTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+  return `generated-qr-${randomIntFromInterval(
+    100,
+    999
+  )}-${stringDateFormat}-${stringDateTime}`;
 }
 
-export async function generateQR(secret: string){
-  const DOMAIN_URI_REDIRECT = process.env.RAILWAY_PUBLIC_DOMAIN || process.env.DOMAIN_URL_REDIRECT // This ENV var is only for localhost
-  const DOMAIN_PROTOCOL = process.env.RAILWAY_PUBLIC_DOMAIN ? 'https://' : 'http://'
-  logger.info({ DOMAIN_URI_REDIRECT })
-  const secretEncoded = encodeURI(secret)
-  const queryTo = `${DOMAIN_PROTOCOL}${DOMAIN_URI_REDIRECT}${validatorRoutes['/validators/validate-form'].path}/?secret=${secretEncoded}` 
-  const QRCodeURL = `${QR_GENERATOR_API_URL}${queryTo}`
-  const QRCOde = await makeRequestRaw(
-    'GET',
-    QRCodeURL,
-    {
-      headers: {
-        'Content-Type': 'image/png'
-      }
-    }
-    )
-  return QRCOde
+export async function generateQR(secret: string) {
+  const DOMAIN_URI_REDIRECT = process.env.FRONTEND_URI;
+  logger.info({ DOMAIN_URI_REDIRECT });
+  const secretEncoded = encodeURI(secret);
+  const queryTo = `${DOMAIN_URI_REDIRECT}/validate-form/?secret=${secretEncoded}`;
+  const QRCodeURL = `${QR_GENERATOR_API_URL}${queryTo}`;
+  const QRCOde = await makeRequestRaw("GET", QRCodeURL, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+  return QRCOde;
 }
