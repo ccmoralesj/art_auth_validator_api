f6cfcc0f29c9db2bfb840429db57bfefa61ee382
Cristian morales <ccmj202@gmail.com.com>
2023-11-29 00:27:58 -0500

    🚀 CORS: Add ORS to known domains

--- src/helpers/consts/index.ts
+++ src/helpers/consts/index.ts
@@ -1,7 +1,13 @@
-import { RouteDeepStructure } from "../../types/index.js"
+import { RouteDeepStructure } from "../../types/index.js";
 
-export const PIN_CONDE_LENGTH = 5
-export const SECRET_TAG_BASE = 'F0TCM'
-export const VALIDATION_PHRASE_TAG_BASE = 'CM'
+export const PIN_CONDE_LENGTH = 5;
+export const SECRET_TAG_BASE = "F0TCM";
+export const VALIDATION_PHRASE_TAG_BASE = "CM";
 
-export const WHITE_LIST_ROUTES: RouteDeepStructure[] = []
+export const WHITE_LIST_ROUTES: RouteDeepStructure[] = [];
+
+export const WHITE_LIST_ORIGINS: string[] = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URI,
+  `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`,
+];
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,53 @@
-import Koa from 'koa';
-import parser from 'koa-parser'
-import dotenv from 'dotenv'; 
-dotenv.config() 
+import Koa from "koa";
+import parser from "koa-parser";
+import cors from "@koa/cors";
+import dotenv from "dotenv";
+dotenv.config();
 // Routers
-import mainRouter from './routes/main/index.js'
-import validatorsRouter from './routes/validators/index.js'
-import accessTokensRouter from './routes/accessToken/index.js'
+import mainRouter from "./routes/main/index.js";
+import validatorsRouter from "./routes/validators/index.js";
+import accessTokensRouter from "./routes/accessToken/index.js";
 
-import { middlewareLogger } from './logger/middleware.js';
-import { logger } from './logger/logger.js';
-import { middlewareAuth } from './services/middlewares/auth.js';
-import './helpers/whiteListRoutes.js'
+import { middlewareLogger } from "./logger/middleware.js";
+import { logger } from "./logger/logger.js";
+import { middlewareAuth } from "./services/middlewares/auth.js";
+import "./helpers/whiteListRoutes.js";
+import { WHITE_LIST_ORIGINS } from "./helpers/consts/index.js";
 
-const API_PORT = process.env.PORT
+const API_PORT = process.env.PORT;
 const app = new Koa();
 
-app.use(parser())
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URI];
+
+const options = {
+  origin(ctx) {
+    const originFound = WHITE_LIST_ORIGINS.find((url) => url === ctx.url);
+    logger.info({ originFound, WHITE_LIST_ORIGINS });
+    if (originFound) {
+      return true;
+    }
+    return false;
+  },
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
+
+app.use(parser());
 
 app.use(middlewareLogger());
 
-app.use(middlewareAuth)
+app.use(middlewareAuth);
 
 // ROUTERS
-app.use(mainRouter.routes()).use(mainRouter.allowedMethods())
-app.use(validatorsRouter.routes()).use(validatorsRouter.allowedMethods())
-app.use(accessTokensRouter.routes()).use(accessTokensRouter.allowedMethods())
+app.use(mainRouter.routes()).use(mainRouter.allowedMethods());
+app.use(validatorsRouter.routes()).use(validatorsRouter.allowedMethods());
+app.use(accessTokensRouter.routes()).use(accessTokensRouter.allowedMethods());
 
 app.listen(API_PORT, () => {
-  logger.info(`🚀 Server ready at: http://localhost:${API_PORT} 🚀\n\t⭐️⭐️⭐️⭐️⭐️`)
+  logger.info(
+    `🚀 Server ready at: http://localhost:${API_PORT} 🚀\n\t⭐️⭐️⭐️⭐️⭐️`
+  );
 });
