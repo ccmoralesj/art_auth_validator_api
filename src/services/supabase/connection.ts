3875aed54fdd108afa19ede28d37e50d7be94fb8
Cristian morales <ccmj202@gmail.com.com>
2023-11-29 22:50:59 -0500

    🐞 Bug: Add supabase connection as a function

--- src/services/supabase/connection.ts
+++ src/services/supabase/connection.ts
@@ -4,7 +4,6 @@ import postgres from "postgres";
 export let supabaseDB: PostgresJsDatabase = undefined;
 export function generateConnection() {
   const connectionString = process.env.DATABASE_URL;
-  console.log(`connection String: ${connectionString}`);
   // Disable prefetch as it is not supported for "Transaction" pool mode
   const client = postgres(connectionString, { prepare: false });
   supabaseDB = drizzle(client);
